0157fcc11f5998cf4842c32edd480df598de0c78
DearInvisible <prettymeohmy@mail.ru>
2021-10-14 20:06:54 +0500

    make women&kids' page responsive with js

--- clths_kids.js
+++ clths_kids.js
@@ -15,64 +15,74 @@ fetch("https://textil.sadaf-med.uz/api/products_by_category", {
     return response.json();
   })
   .then(function (data) {
-    const res5 = data.map((product) => {
-      const markup5 = `
+    function myFunction(x) {
+      if (x.matches) {
+        const res4 = data.map((product) => {
+          const markup4 = `
       <div class="d-flex justify-content-center mt-5">
-      <div
-        class="card-group mx-5 p-4 cardsborder mt-5 d-flex align-items-center"
-      >
-        <div class="border-end firstcard">
-          <img
-            src="https://textil.sadaf-med.uz/${product.images}"
-            class="card-img-top mt-3"
-          />
-        </div>
+  <div class="card-group mx-5 p-4 cardsborder mt-5 d-flex align-items-center">
+    <div class="border-end firstcard">
+      <img
+        src="https://textil.sadaf-med.uz/${product.images}"
+        class="card-img-top mt-3 shrt"
+      />
+      <h5 class="text-muted shirt_txt_short">${product.slug}</h5>
+    </div>
 
-        <div class="border-end secondcard inter">
-          <h3>${product.name}</h3>
-          <br />
-          <h6 class="text-muted">
-            ${product.description}
-          </h6>
-        </div>
+    <div class="border-end secondcard inter">
+      <h3>${product.name}</h3>
+      <br />
+      <h6 class="text-muted">${product.description}</h6>
+    </div>
 
-        <div class="thirdcard">
-          <div class="row d-flex justify-content-center pt-1">
-            <div>
-              <h5 class="bluetxt">Available: ${product.quantity} Pcs</h5>
-              <a href="#"
-                ><img id="strs1" src="./textile/imgs/stargold.svg"
-              /></a>
-              <a href="#"
-                ><img id="strs1" src="./textile/imgs/stargold.svg"
-              /></a>
-              <a href="#"
-                ><img id="strs1" src="./textile/imgs/stargold.svg"
-              /></a>
-              <a href="#"
-                ><img id="strs1" src="./textile/imgs/stargold.svg"
-              /></a>
-              <a href="#"
-                ><img id="strs1" src="./textile/imgs/starsilver.svg"
-              /></a>
-              <p class="inter">${product.short_des}</p>
-              <h4 class="card-title mt-5"><strong>${product.regular_price}</strong></h4>
-              <div class="d-flex">
-                <a href="#" class="btn btn-primary btnbuy blueclr radius mt-1"
-                  ><span class="loremsize"><strong>BUY NOW</strong></span></a
-                >
-                <div class="circle ms-2 grey_border circlesize mt-1">
-                  <a href="#"><img src="./textile/imgs/hrtnclr.png" /></a>
-                </div>
-              </div>
+    <div class="thirdcard">
+      <div class="row d-flex justify-content-center pt-1">
+        <div class="thrdcrd">
+          <h5 class="bluetxt available">Available: ${product.quantity} Pcs</h5>
+          <a href="#">
+            <img id="strs1" src="./textile/imgs/stargold.svg" />
+          </a>
+          <a href="#">
+            <img id="strs1" src="./textile/imgs/stargold.svg" />
+          </a>
+          <a href="#">
+            <img id="strs1" src="./textile/imgs/stargold.svg" />
+          </a>
+          <a href="#">
+            <img id="strs1" src="./textile/imgs/stargold.svg" />
+          </a>
+          <a href="#">
+            <img id="strs1" src="./textile/imgs/starsilver.svg" />
+          </a>
+          <p class="inter short_desc">${product.short_des}</p>
+          <h4 class="card-title mt-5 shirt_prc">
+            <strong>${product.regular_price}</strong>
+          </h4>
+          <div class="d-flex">
+            <a href="#" class="btn btn-primary btnbuy blueclr radius mt-1">
+              <span class="loremsize">
+                <strong>BUY NOW</strong>
+              </span>
+            </a>
+            <div class="circle ms-2 grey_border circlesize mt-1">
+              <a href="#">
+                <img class="hrt" src="./textile/imgs/hrtnclr.png" />
+              </a>
             </div>
           </div>
         </div>
       </div>
     </div>
+  </div>
+</div>
     `;
-      return markup5;
-    });
+          return markup4;
+        });
 
-    clths_kids_cntnr.innerHTML = res5.join("");
+        clths_kids_cntnr.innerHTML = res4.join("");
+      }
+    }
+
+    let x = window.matchMedia("(min-width: 990px)");
+    myFunction(x);
   });
